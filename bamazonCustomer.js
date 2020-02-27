72611ade5bf747929c73f444b0965345449140bc
Jennifer Moundanos <jmoundanos@gmail.com>
2020-02-26 21:51:41 -0600

    add table package and display invetory function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,16 +1,12 @@
 var inquirer = require("inquirer");
-var mysql = require("sql");
+var mysql = require("mysql");
+var Table = require("cli-table3");
 
+//create a connection and store it in a variable
 var connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
     port: 3306,
-  
-    // Your username
     user: "root",
-  
-    // Your password
     password: "daphnemax12",
     database: "bamazon"
   });
@@ -18,7 +14,28 @@ var connection = mysql.createConnection({
   // connect to the mysql server and sql database
   connection.connect(function(err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    start();
-  });
+    console.log("connected on " + connection.threadId);
+    showAllProducts();
+});
   
+var showAllProducts = function(){
+    var query = "SELECT * FROM products";
+    connection.query(query, function(err, res){
+        if(err) throw err;
+        var table = new Table({
+            head: ["Item ID", "Product Name", "Department", "Price", "Quantity"],
+            colWidths: [10, 25, 20, 6, 5]
+        });
+        for(var i = 0; i < res.length; i++){
+            var itemId = res[i].item_id,
+            productName = res[i].product_name,
+            departmentName = res[i].department_name,
+            price = res[i].price,
+            stockQuantity = res[i].stock_quantity;
+
+      table.push(
+          [itemId, productName, departmentName, price, stockQuantity]);
+        }
+        console.log(table.toString());
+    });
+}     
